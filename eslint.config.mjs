c361f817a1db568b99af49dd1c64721154385c90
zaki-20 <zakibutt199@gmail.com>
2024-11-22 21:42:51 +0500

    test lint-staged (10)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -18,6 +18,7 @@ export default tseslint.config(
     },
     rules: {
       'no-console': 'off',
+      'dot-notation': 'error',
     },
   },
 );
---
+++ src/server.ts
@@ -0,0 +1,6 @@
+// test dot notation fix
+const a = { b: 1 };
+let user = a.b;
+
+user = 2;
+console.log(user);
