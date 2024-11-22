a2c37fb32ca23586154bf6db1288c6091b5b0eb8
zaki-20 <zakibutt199@gmail.com>
2024-11-22 19:48:28 +0500

    add eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.recommendedTypeChecked,
+  {
+    ignores: ['dist', 'node_modules', 'eslint.config.mjs'],
+  },
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      'no-console': 'off',
+    },
+  },
+);
