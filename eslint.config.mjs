4c138b852d16098022d063129b248fead3ab3480
zaki-20 <zakibutt199@gmail.com>
2024-11-23 03:24:18 +0500

    test

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -17,8 +17,8 @@ export default tseslint.config(
       },
     },
     rules: {
-    //   "no-console": "error",
-    //   "dot-notation": "error",
+      //   "no-console": "error",
+      //   "dot-notation": "error",
     },
   },
 );
